aef40ad6c25bf222021fe6c31eb2e05067f6b9fe
Brillianta Zayyan Muhamad <94041454+zzayyan@users.noreply.github.com>
2022-11-20 08:17:57 +0700

    Add files via upload

---
+++ bayes.pl
@@ -0,0 +1,37 @@
+bayes(X,C) :-
+    probs(X,W),
+    max(W,C/_).
+
+probs(X,W) :-
+    setof(C,N^E^prediction(N,C,E),Cs),
+    findall(C/P,(member(C,Cs),
+                 cond_prob(X,C,PL),
+                 class_prob(C,PC),
+                 mult(PL,PS),
+                 P is PS*PC),W).
+
+cond_prob([],_,[]).
+cond_prob([AV|L],C,[AV/P|PL]) :-
+    findall(I,prediction(I,C,_),All),
+    length(All,N),
+    findall(I,(prediction(I,C,AVL),member(AV,AVL)),W),
+    length(W,M),
+    P is M/N,
+    cond_prob(L,C,PL).
+
+class_prob(C,PC) :-
+    findall(I,prediction(I,_,_),All),
+    length(All,N),
+    findall(I,prediction(I,C,_),W),
+    length(W,M),
+    PC is M/N.
+    
+max([X],X) :- !.
+max([X/M|T],Y/N) :- 
+    max(T,Z/K), 
+    (M>K,Y/N=X/M;Y/N=Z/K), !.
+
+mult([],1) :- !.
+mult([_/P|L],Ps) :-
+    mult(L,P1),
+    Ps is P1*P.
